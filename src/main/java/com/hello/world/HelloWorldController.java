965a05a4eef4d68c3d02fd68634daedb192e00d7
asinchuk <andriysinchuk@gmail.com>
2019-10-16 14:09:06 +0300

    1) add expose actuator endpoints in .properties file 2) add request method to /hello endpoint

--- src/main/java/com/hello/world/HelloWorldController.java
+++ src/main/java/com/hello/world/HelloWorldController.java
@@ -1,13 +1,14 @@
 package com.hello.world;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
 
-    @RequestMapping("/hello")
+    @RequestMapping(value = "/hello", method = RequestMethod.PUT)
     public String sayHello(@RequestParam(value = "name") String name) {
         return "Hello " + name + "!";
     }
