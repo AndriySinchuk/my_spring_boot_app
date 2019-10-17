5dc43bec2caec69b2c3fab904c7018c06b72f7b6
asinchuk <andriysinchuk@gmail.com>
2019-10-17 15:02:55 +0300

    resolve problem with @param

--- src/main/java/com/hello/world/controllers/CatController.java
+++ src/main/java/com/hello/world/controllers/CatController.java
@@ -4,6 +4,7 @@ import com.hello.world.cat.Cat;
 import com.hello.world.cat.CatStub;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -14,5 +15,13 @@ public class CatController {
     public List<Cat> listAllCats() {
         return CatStub.allCats();
     }
+
+//    @RequestMapping(value = "/cats/add", consumes = "application/json", method = RequestMethod.POST)
+//    public List addCat(@RequestParam("catName") String catName,
+//                       @RequestParam("catColor") String catColor,
+//                       @RequestParam("catAge") String catAge) {
+//
+//        return CatStub.allCats() - add addCat(S s,S s, S ,) method;
+//    }
 
 }
