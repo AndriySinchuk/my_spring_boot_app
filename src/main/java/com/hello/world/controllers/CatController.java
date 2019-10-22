efca6e6b2696fcf490c555bc1cdac2e3d979cc4c
asinchuk <andriysinchuk@gmail.com>
2019-10-22 11:50:35 +0300

    deletion of the unnecessary return types

--- src/main/java/com/hello/world/controllers/CatController.java
+++ src/main/java/com/hello/world/controllers/CatController.java
@@ -3,7 +3,6 @@ package com.hello.world.controllers;
 import com.hello.world.cat.Cat;
 import com.hello.world.cat.CatRepo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,32 +18,28 @@ public class CatController {
     }
 
     @RequestMapping(value = "/cat",
-            method = RequestMethod.POST,
-            produces = {MediaType.APPLICATION_JSON_VALUE})
+            method = RequestMethod.POST)
     @ResponseBody
     public Cat createCat(@RequestBody Cat cat) {
         return catRepo.addCat(cat);
     }
 
     @RequestMapping(value = "/cat/{cat_number}",
-            method = RequestMethod.GET,
-            produces = {MediaType.APPLICATION_JSON_VALUE})
+            method = RequestMethod.GET)
     @ResponseBody
     public Cat readCat(@PathVariable("cat_number") Integer catNumber) {
         return catRepo.getCat(catNumber);
     }
 
     @RequestMapping(value = "/cat",
-            method = RequestMethod.PUT,
-            produces = {MediaType.APPLICATION_JSON_VALUE})
+            method = RequestMethod.PUT)
     @ResponseBody
     public Cat updateCat(@RequestBody Cat cat) {
         return catRepo.updateCat(cat);
     }
 
     @RequestMapping(value = "/cat/{cat_number}",
-            method = RequestMethod.DELETE,
-            produces = {MediaType.APPLICATION_JSON_VALUE})
+            method = RequestMethod.DELETE)
     @ResponseBody
     public void deleteCat(@PathVariable("cat_number") Integer catNumber) {
         catRepo.deleteCat(catNumber);
